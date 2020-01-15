9270c6ec01619bd5179c07d4e14d3dc13ec04a43
kmm68 <kmm68@maroon22.cs.calvin.edu>
2020-01-15 09:09:14 -0500

    upload code

---
+++ Mon13/simple.js
@@ -0,0 +1,8 @@
+d3.select("svg")
+    .style("background-color", "skyblue")
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", 600) 
+    .attr("height", 200)
+    .style("fill","red")
